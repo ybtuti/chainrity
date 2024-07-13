c4476a5203cf65aa61dd94b4c87082385e91daa6
ybtuti <samuelmsuu@gmail.com>
2024-07-13 19:27:31 +0300

    refactor: Rename DonationPlatform contract to Chainrity
    This commit renames the DonationPlatform contract to Chainrity to better reflect its purpose as a platform for charitable donations. The contract is refactored to update all references to the old name and ensure consistency throughout the codebase. This renaming improves the clarity and understanding of the contract's functionality.

--- src/Chainrity.sol
+++ src/Chainrity.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.19;
 import "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
 import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-contract DonationPlatform is AutomationCompatibleInterface {
+contract Chainrity is AutomationCompatibleInterface {
     // Donation struct to hold details about each donation
     struct Donation {
         address payable donor;

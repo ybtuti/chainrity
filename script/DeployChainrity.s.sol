d13235364f5c5879216c609647c26d8ea2934657
ybtuti <samuelmsuu@gmail.com>
2024-08-17 14:25:19 +0300

    feat: Add Chainrity contract deployment script
    The commit adds a script for deploying the Chainrity contract. It includes the necessary imports and a `run` function that deploys the contract and returns the instance.

--- script/DeployChainrity.s.sol
+++ script/DeployChainrity.s.sol
@@ -2,11 +2,21 @@
 pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Chainrity} from "src/Chainrity.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 
-contract CounterScript is Script {
-    function setUp() public {}
+contract DeployChainrity is Script {
+    //   function setUp() public {}
 
-    function run() public {
-        vm.broadcast();
+    function run() public returns (Chainrity) {
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        Chainrity chainrity = new Chainrity(ethUsdPriceFeed);
+        vm.stopBroadcast();
+        return chainrity;
+
+        //       console.log("Chainrity contract address: ", chainrity);
     }
 }
